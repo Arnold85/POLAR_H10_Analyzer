f6bc4e452c1e6e34d1ca681be7d764b48020a6bc
Arnold85 <arne.broedel@gmail.com>
2025-09-20 00:01:01 +0200

    Set zero card margins in themes

---
+++ lib/src/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.light,
+          seedColor: const Color(0xFF0066FF),
+        ),
+        cardTheme: const CardTheme(margin: EdgeInsets.zero),
+      );
+
+  static ThemeData get dark => ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: const Color(0xFF0066FF),
+        ),
+        cardTheme: const CardTheme(margin: EdgeInsets.zero),
+      );
+}
